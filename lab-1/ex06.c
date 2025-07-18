71755a9e6825711946bcb4c30dbc4efa4477c2f7
Noeh-mi <68011374@kmitl.ac.th>
2025-07-18 04:06:33 +0000

    Compleate

---
+++ lab-1/ex06.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int number = 69;
+    printf("Decimal: %d\n", number);
+    printf("Octal: %o\n", number);
+    printf("Hexadecimal: %X\n", number);
+    return 0;
+}
